1fc5dbcdc4bad375b3e4baa4cd4398adc4c8728d
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-07-20 23:52:54 -0400

    completed the project without the stretch

--- index.js
+++ index.js
@@ -126,3 +126,21 @@ function scoreboard(callScore, callInn, num) {
   return `Final Score:${totalScore.Home} - ${totalScore.Away}`
 }
 console.log(scoreboard(finalScore,inning,9))
+//
+
+function personalDice(name){
+  return function(){
+      // generate random number between 1 and 6
+    const newRoll = Math.floor(Math.random() * 6);
+    console.log(`${name} rolled a ${newRoll}`)
+  }
+}
+
+const dansRoll = personalDice("Dan");
+
+const zoesRoll = personalDice("Zoe");
+
+
+dansRoll();
+dansRoll();
+//
